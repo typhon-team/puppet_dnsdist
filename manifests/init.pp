713ec01d92b98d13c2dcebf1799307a6775b3025
Geoff Robertson <gr@hetzner.co.za>
2016-12-01 13:52:40 +0200

    Add option to manage the service for HA setups

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,10 +34,15 @@ class dnsdist (
   $listen_addresses = $dnsdist::params::listen_addresses,
   $version          = $dnsdist::params::version,
   $distribution     = $dnsdist::params::distribution,
-  $server_policy    = $dnsdist::params::server_policy
+  $server_enabled   = $dnsdist::params::service_enabled,
+  $server_ensure    = $dnsdist::params::service_ensure,
+  $server_policy    = $dnsdist::params::server_policy,
 ) inherits ::dnsdist::params {
 
-  include ::dnsdist::service
+  class { '::dnsdist::service':
+    enabled => $server_enabled,
+    ensure  => $server_ensure
+  }
 
   class { '::dnsdist::package':
     distribution => $distribution,

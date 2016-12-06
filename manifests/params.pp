fcd5976a0596ffda21c0affa3cc8f87f1ac17542
Geoff Robertson <gr@hetzner.co.za>
2016-12-06 16:34:07 +0200

    Add recursion

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,6 +14,7 @@ class dnsdist::params {
   $control_socket   = '127.0.0.1'
   $distribution     = 'ubuntu'
   $listen_addresses = '127.0.0.1'
+  $number_of_cpus   = 1
   $service_enabled  = true
   $service_ensure   = true
   $server_policy    = 'firstAvailable'

4b1aeba4daa8d600ee9b73b6635bda00f6851882
Maxence Dunnewind <maxence@typhon.com>
2018-10-15 17:20:55 +0200

    add rules for dnsdist

--- manifests/maintenance.pp
+++ manifests/maintenance.pp
@@ -1,4 +1,4 @@
-define puppet_dnsdist::maintenance ($dynblocks = []) {
+define dnsdist::maintenance ($dynblocks = []) {
   concat::fragment { "maintenance-${title}":
     target  => "/etc/dnsdist/dnsdist.conf",
     content => template('dnsdist/maintenance.erb'),

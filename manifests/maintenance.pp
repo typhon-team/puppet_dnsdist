87ade0c228662ebd52144d99986067727f68c4ba
Maxence Dunnewind <maxence@typhon.com>
2018-10-15 17:12:30 +0200

    add maintenance

---
+++ manifests/maintenance.pp
@@ -0,0 +1,7 @@
+define puppet_dnsdist::maintenance ($dynblocks = []) {
+  concat::fragment { "maintenance-${title}":
+    target  => "/etc/dnsdist/dnsdist.conf",
+    content => template('dnsdist/maintenance.erb'),
+    order   => "45"
+  }
+}

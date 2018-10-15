dddab4908ea683545608ff3eca86f47a63437d0a
Maxence Dunnewind <maxence@typhon.com>
2018-10-15 17:39:59 +0200

    add rules for dnsdist

--- manifests/maintenance.pp
+++ manifests/maintenance.pp
@@ -2,6 +2,6 @@ define dnsdist::maintenance ($dynblocks = []) {
   concat::fragment { "maintenance-${title}":
     target  => "/etc/dnsdist/dnsdist.conf",
     content => template('dnsdist/maintenance.erb'),
-    order   => "45"
+    order   => "50"
   }
 }
